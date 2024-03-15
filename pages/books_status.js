00b6c934c5ca78ff56239731354fb1bfad10f531
Sarthak Kagliwal <sarthak55k@Sarthaks-MacBook-Air.local>
2024-03-15 18:53:46 -0400

    done

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,14 @@
 let BookInstance = require('../models/bookinstance');
 
 exports.show_all_books_status = function(res) {
-  return res.send([]);
-}
+
+  BookInstance.find()
+    .populate('book')
+    .exec()
+    .then(list_bookinstances => {
+      res.send(list_bookinstances.map(function(bookInstance) {
+        return bookInstance.book.title + " : " + bookInstance.status
+      }));
+    })
+    .catch(err => res.send('Book Status not found'));
+};
